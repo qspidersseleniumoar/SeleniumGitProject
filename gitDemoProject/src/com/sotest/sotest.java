7a33891ae32c17c0d4d56dd2d9697342e5079503
qsp-lab3 <qsp-lab3@qsp-lab3-PC>
2018-02-07 01:27:28 +0530

    commit 6

---
+++ gitDemoProject/src/com/sotest/sotest.java
@@ -0,0 +1,20 @@
+package com.sotest;
+
+import org.testng.annotations.Test;
+
+public class sotest {
+	
+	
+	@Test
+	public void createDemoTest() {
+		System.out.println("step 1");
+		
+		System.out.println("step 2");
+		
+		System.out.println("step 3");
+		
+		System.out.println("step 4");
+	}
+
+
+}
