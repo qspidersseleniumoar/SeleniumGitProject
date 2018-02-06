08a566307f5591094c9185a775ad799d9bac623d
qsp-lab3 <qsp-lab3@qsp-lab3-PC>
2018-02-07 01:30:53 +0530

    commit 7

---
+++ gitDemoProject/src/com/potest/potest.java
@@ -0,0 +1,17 @@
+package com.potest;
+
+import org.testng.annotations.Test;
+
+public class potest {
+	@Test
+	public void createDemoTest() {
+		System.out.println("step 1");
+		
+		System.out.println("step 2");
+		
+		System.out.println("step 3");
+		
+		System.out.println("step 4");
+	}
+
+}
